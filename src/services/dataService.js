280b76065c25db092b98d62e365c1062f0c7ff97
Frane Zunic <franezunic7@gmail.com>
2024-07-05 10:28:40 +0200

    chore: add missing file

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,10 +1,10 @@
-const axios = require('axios')
+const axiosInstance = require('../../api/axiosInstance')
 
-const API_URL = 'https://chat.codeasy.com/api/public/job-application'
+const API_URL = '/job-application'
 
 const fetchRoutesData = async() => {
   try {
-    const response = await axios.get(API_URL)
+    const response = await axiosInstance.get(API_URL)
     return response.data
   } catch (error) {
     console.error('Error fetching data from API:', error)
